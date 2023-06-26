700e9c6ec6a4c5ed0aaae3ce9eed19d7f8613759
enjoi93 <dream.cloud936@gmail.com>
2023-06-26 17:40:37 -0600

    final nav push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,33 +46,27 @@ export default function Navbar() {
 
     const [mode, setMode] = useThemeSwitcher();
     const [open, setOpen] = useState(false);
+    const [navbar, setNavbar] = useState(false)
 
     const handleClick = () => {
         setOpen(!open)
     }
  
-    useEffect( () => {
-        const body = document.body
-        let lastScroll = 0
+    const changeBackground = () => {
+        if (window.scrollY >= 20) {
+            setNavbar(true)
+        } else {
+            setNavbar(false)
+        }
+    }
 
-        window.addEventListener("scroll", () => {
-            const currentScroll = window.scrollY
-
-            if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-                body.classList.add("scroll-down")
-            }
-
-            if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-                body.classList.remove("scroll-down")
-            }
-
-            lastScroll = currentScroll
-        })
-    })
+    window.addEventListener('scroll', changeBackground)
 
     return (
         <header>
-            <nav className="fixed top-0 left-0 z-10 flex w-full filter backdrop-blur-lg dark:text-[#191835] px-10 py-2 items-center transition-all duration-300 ease-in-out">
+            <nav className={`fixed top-0 left-0 z-10 flex w-full filter backdrop-blur-lg dark:text-[#191835] px-10 py-2 items-center transition-all duration-300 ease-in-out
+            ${navbar ? 'bg-light dark:bg-[#3192DE] transform transition-all duration-200' : null}
+            `}>
 
                 <div className='w-full flex justify-start'>
                     <Logo />
