03cb754f9dafd3d927f4d8ac6725fbac8aaf6669
enjoi93 <dream.cloud936@gmail.com>
2023-07-15 14:47:02 -0600

    url issue fix

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,8 +2,9 @@
 
 import { useScroll, motion } from 'framer-motion'
 import { useRef } from 'react'
+import Link from 'next/link'
 
-const Details = ({position, company, companyLink, time, address, work}) => {
+const Details = ({position, company, url, time, address, work}) => {
     return (
         <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col text-left">
 
@@ -13,14 +14,13 @@ const Details = ({position, company, companyLink, time, address, work}) => {
             viewport={{once: true, amount: 0.5}}
             transition={{duration: 0.5, type: "spring"}}
             >
-                <h3 className="capitalize font-bold text-xl lg:text-2xl">{position}&nbsp;<a href={companyLink} target="_blank" className="text-red-400 dark:text-[#7876FD] capitalize">@&nbsp;{company}</a></h3>
+                <h3 className="capitalize font-bold text-xl lg:text-2xl">{position}&nbsp;<a href={url} target="_blank" className="text-red-400 dark:text-[#7876FD] capitalize">@&nbsp;{company}</a></h3>
                 <span className="capitalize font-medium text-dark/75 dark:text-light">
                     {time} | {address}
                 </span>
                 <p className="font-medium text-sm lg:text-base w-full">
                     {work}
                 </p>
-
             </motion.div>
 
         </li>
@@ -55,19 +55,18 @@ export default function Experience() {
 
                     <ul className="w-full flex flex-col items-start justify-between ml-4">
                         <Details
-                            position="Career Technical Education Ambassador" company="CWI" companyLink="www.cwi.edu" time="2022 - Present" address="Nampa, ID" work="Working in a small team to provide insight and assistance of entering CTE vocations such as HVAC, Automotive, etc. to dual
+                            position="Career Technical Education Ambassador" company="CWI" url='https://cwi.edu/' time="2022 - Present" address="Nampa, ID" work="Working in a small team to provide insight and assistance of entering CTE vocations such as HVAC, Automotive, etc. to dual
                             credit/high school students. Provide full range of services, includes event hosting, event scheduling, B2P data logging via
                             Insightly, and service marketing."                        
                         />
                         <Details
-                            position="Project Manager" company="Final Mile Solar" companyLink="www.finalmilesolar.com" time="2019 - 2022" address="Arlington, TX" work="Inhouse & field management. Responsible for training, route scheduling, and client relations."
+                            position="Project Manager" company="Final Mile Solar" url='https://www.finalmilesolar.com/' time="2019 - 2022" address="Arlington, TX" work="Inhouse & field management. Responsible for training, route scheduling, and client relations."
                         />
                         <Details
-                            position="Web Developer Intern" company="MindHome" companyLink="www.mindhome.co" time="2022 - 2023" address="Remote" work="Assisted with web design and prepared prototypes to be
+                            position="Web Developer Intern" company="MindHome" url='https://www.mindhome.co/' time="2022 - 2023" address="Remote" work="Assisted with web design and prepared prototypes to be
                             hosted on the official site"
                         />
                     </ul>
-
             </div>
         </div>
     )
